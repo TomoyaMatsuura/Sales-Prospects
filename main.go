9028f68152b85c4da59aa9d16013fa63d37629c3
TomoyaMatsuura <tomoya_matsuura@mj.makita.co.jp>
2023-06-29 18:55:24 +0900

    mod

--- main.go
+++ main.go
@@ -88,15 +88,59 @@ func getCellReport(branchFile *excelize.File, comment [10]string, sheet string) 
 	return salesReport, qtyReport, commentReport
 }
 
+// 売上予想出力関数
+func setProspect(sumFile *excelize.File, column string, rowNumber int, salesResult string, salesProspect string, qtyProspect string, addNumber int) {
+	if salesResult != "" {
+		sumFile.SetCellValue("変数", column+calculateResult(rowNumber, 0), salesResult)
+	}
+	if salesProspect != "" {
+		sumFile.SetCellValue("変数", column+calculateProspect(rowNumber, 9, addNumber), salesProspect)
+	}
+
+	if qtyProspect != "" {
+		sumFile.SetCellValue("変数", column+calculateProspect(rowNumber, 10, addNumber), qtyProspect)
+	}
+}
+
+func setProspect2(sumFile *excelize.File, column string, rowNumber int, salesResult2 string, salesProspect2 string, qtyProspect2 string, addNumber int) {
+	if salesResult2 != "" {
+		sumFile.SetCellValue("変数", column+calculateResult(rowNumber, 26), salesResult2)
+	}
+
+	if salesProspect2 != "" {
+		sumFile.SetCellValue("変数", column+calculateProspect(rowNumber, 35, addNumber), salesProspect2)
+	}
+
+	if qtyProspect2 != "" {
+		sumFile.SetCellValue("変数", column+calculateProspect(rowNumber, 36, addNumber), qtyProspect2)
+	}
+
+}
+
+func setReport(sumFile *excelize.File, column string, rowNumber int, salesReport string, qtyReport string, addNumber int) {
+	if salesReport != "" {
+		sumFile.SetCellValue("変数", column+calculateProspect(rowNumber, 52, addNumber), salesReport)
+	}
+
+	if qtyReport != "" {
+		sumFile.SetCellValue("変数", column+calculateProspect(rowNumber, 53, addNumber), qtyReport)
+	}
+
+}
+
 // コメント出力関数
 func setComment(sumFile *excelize.File, commentNumber int, commentValue [10]string, sheetName string) {
 	for m := 0; m < 10; m++ {
-		sumFile.SetCellValue(sheetName, calculateComment(commentNumber+m), commentValue[m])
+		if commentValue[m] != "" {
+			sumFile.SetCellValue(sheetName, calculateComment(commentNumber+m), commentValue[m])
+		}
 	}
 }
 
 func main() {
 	branch := [7]string{"MBR", "MMX", "MCL", "MAR", "MLA", "MPE", "MCO"}
+
+	// 予想/速報のコメントはA18->A27なので、関数で取得する用の配列
 	comment := [10]string{"A18", "A19", "A20", "A21", "A22", "A23", "A24", "A25", "A26", "A27"}
 
 	loggingSettings("ログ.log")
@@ -167,30 +211,10 @@ func main() {
 
 		// 1回目の売上予想取得
 		salesResult, salesProspect, qtyProspect, commentProspect := getCellProspect(branchFile, comment, "売上予想1回目")
-
 		// 2回目の売上予想取得
 		salesResult2, salesProspect2, qtyProspect2, commentProspect2 := getCellProspect(branchFile, comment, "売上予想2回目")
-
 		// 売上速報取得
-		// salesReport, err := branchFile.GetCellValue("速報値", "B6")
-		// if err != nil {
-		// 	log.Println(err)
-		// }
-		// qtyReport, err := branchFile.GetCellValue("速報値", "D6")
-		// if err != nil {
-		// 	log.Println(err)
-		// }
-		// commentReport := [10]string{}
-		// for l := 0; l < 10; l++ {
-		// 	comment, err := branchFile.GetCellValue("速報値", comment[l])
-		// 	if err != nil {
-		// 		log.Println(err)
-		// 		return
-		// 	}
-		// 	commentReport[l] = comment
-		// }
 		salesReport, qtyReport, commentReport := getCellReport(branchFile, comment, "速報値")
-
 		// 転記先ファイルを開く
 		sumFile, err := excelize.OpenFile("Summary.xlsx")
 		if err != nil {
@@ -213,148 +237,88 @@ func main() {
 		switch month {
 
 		case "April":
-			sumFile.SetCellValue("変数", "D"+calculateResult(rowNumber, 0), salesResult)
-			sumFile.SetCellValue("変数", "D"+calculateProspect(rowNumber, 9, addNumber), salesProspect)
-			sumFile.SetCellValue("変数", "D"+calculateProspect(rowNumber, 10, addNumber), qtyProspect)
-			sumFile.SetCellValue("変数", "D"+calculateResult(rowNumber, 26), salesResult2)
-			sumFile.SetCellValue("変数", "D"+calculateProspect(rowNumber, 35, addNumber), salesProspect2)
-			sumFile.SetCellValue("変数", "D"+calculateProspect(rowNumber, 36, addNumber), qtyProspect2)
-			sumFile.SetCellValue("変数", "O"+calculateProspect(rowNumber, 52, addNumber), salesReport)
-			sumFile.SetCellValue("変数", "O"+calculateProspect(rowNumber, 53, addNumber), qtyReport)
+			setProspect(sumFile, "D", rowNumber, salesResult, salesProspect, qtyProspect, addNumber)
+			setProspect2(sumFile, "D", rowNumber, salesResult2, salesProspect2, qtyProspect2, addNumber)
+			setReport(sumFile, "O", rowNumber, salesReport, qtyReport, addNumber)
 			setComment(sumFile, commentNumber, commentProspect, month+" 1st")
 			setComment(sumFile, commentNumber, commentProspect2, month+" 2nd")
 			setComment(sumFile, commentNumber, commentReport, "March Report")
 
 		case "May":
-			sumFile.SetCellValue("変数", "E"+calculateResult(rowNumber, 0), salesResult)
-			sumFile.SetCellValue("変数", "E"+calculateProspect(rowNumber, 9, addNumber), salesProspect)
-			sumFile.SetCellValue("変数", "E"+calculateProspect(rowNumber, 10, addNumber), qtyProspect)
-			sumFile.SetCellValue("変数", "E"+calculateResult(rowNumber, 26), salesResult2)
-			sumFile.SetCellValue("変数", "E"+calculateProspect(rowNumber, 35, addNumber), salesProspect2)
-			sumFile.SetCellValue("変数", "E"+calculateProspect(rowNumber, 36, addNumber), qtyProspect2)
-			sumFile.SetCellValue("変数", "D"+calculateProspect(rowNumber, 52, addNumber), salesReport)
-			sumFile.SetCellValue("変数", "D"+calculateProspect(rowNumber, 53, addNumber), qtyReport)
+			setProspect(sumFile, "E", rowNumber, salesResult, salesProspect, qtyProspect, addNumber)
+			setProspect2(sumFile, "E", rowNumber, salesResult2, salesProspect2, qtyProspect2, addNumber)
+			setReport(sumFile, "D", rowNumber, salesReport, qtyReport, addNumber)
 			setComment(sumFile, commentNumber, commentProspect, month+" 1st")
 			setComment(sumFile, commentNumber, commentProspect2, month+" 2nd")
 			setComment(sumFile, commentNumber, commentReport, "April Report")
 		case "June":
-			sumFile.SetCellValue("変数", "F"+calculateResult(rowNumber, 0), salesResult)
-			sumFile.SetCellValue("変数", "F"+calculateProspect(rowNumber, 9, addNumber), salesProspect)
-			sumFile.SetCellValue("変数", "F"+calculateProspect(rowNumber, 10, addNumber), qtyProspect)
-			sumFile.SetCellValue("変数", "F"+calculateResult(rowNumber, 26), salesResult2)
-			sumFile.SetCellValue("変数", "F"+calculateProspect(rowNumber, 35, addNumber), salesProspect2)
-			sumFile.SetCellValue("変数", "F"+calculateProspect(rowNumber, 36, addNumber), qtyProspect2)
-			sumFile.SetCellValue("変数", "E"+calculateProspect(rowNumber, 52, addNumber), salesReport)
-			sumFile.SetCellValue("変数", "E"+calculateProspect(rowNumber, 53, addNumber), qtyReport)
+			setProspect(sumFile, "F", rowNumber, salesResult, salesProspect, qtyProspect, addNumber)
+			setProspect2(sumFile, "F", rowNumber, salesResult2, salesProspect2, qtyProspect2, addNumber)
+			setReport(sumFile, "E", rowNumber, salesReport, qtyReport, addNumber)
 			setComment(sumFile, commentNumber, commentProspect, month+" 1st")
 			setComment(sumFile, commentNumber, commentProspect2, month+" 2nd")
 			setComment(sumFile, commentNumber, commentReport, "May Report")
 
 		case "July":
-			sumFile.SetCellValue("変数", "G"+calculateResult(rowNumber, 0), salesResult)
-			sumFile.SetCellValue("変数", "G"+calculateProspect(rowNumber, 9, addNumber), salesProspect)
-			sumFile.SetCellValue("変数", "G"+calculateProspect(rowNumber, 10, addNumber), qtyProspect)
-			sumFile.SetCellValue("変数", "G"+calculateResult(rowNumber, 26), salesResult2)
-			sumFile.SetCellValue("変数", "G"+calculateProspect(rowNumber, 35, addNumber), salesProspect2)
-			sumFile.SetCellValue("変数", "G"+calculateProspect(rowNumber, 36, addNumber), qtyProspect2)
-			sumFile.SetCellValue("変数", "F"+calculateProspect(rowNumber, 52, addNumber), salesReport)
-			sumFile.SetCellValue("変数", "F"+calculateProspect(rowNumber, 53, addNumber), qtyReport)
+			setProspect(sumFile, "G", rowNumber, salesResult, salesProspect, qtyProspect, addNumber)
+			setProspect2(sumFile, "G", rowNumber, salesResult2, salesProspect2, qtyProspect2, addNumber)
+			setReport(sumFile, "F", rowNumber, salesReport, qtyReport, addNumber)
 			setComment(sumFile, commentNumber, commentProspect, month+" 1st")
 			setComment(sumFile, commentNumber, commentProspect2, month+" 2nd")
 			setComment(sumFile, commentNumber, commentReport, "June Report")
 		case "August":
-			sumFile.SetCellValue("変数", "H"+calculateResult(rowNumber, 0), salesResult)
-			sumFile.SetCellValue("変数", "H"+calculateProspect(rowNumber, 9, addNumber), salesProspect)
-			sumFile.SetCellValue("変数", "H"+calculateProspect(rowNumber, 10, addNumber), qtyProspect)
-			sumFile.SetCellValue("変数", "H"+calculateResult(rowNumber, 26), salesResult2)
-			sumFile.SetCellValue("変数", "H"+calculateProspect(rowNumber, 35, addNumber), salesProspect2)
-			sumFile.SetCellValue("変数", "H"+calculateProspect(rowNumber, 36, addNumber), qtyProspect2)
-			sumFile.SetCellValue("変数", "G"+calculateProspect(rowNumber, 52, addNumber), salesReport)
-			sumFile.SetCellValue("変数", "G"+calculateProspect(rowNumber, 53, addNumber), qtyReport)
+			setProspect(sumFile, "H", rowNumber, salesResult, salesProspect, qtyProspect, addNumber)
+			setProspect2(sumFile, "H", rowNumber, salesResult2, salesProspect2, qtyProspect2, addNumber)
+			setReport(sumFile, "G", rowNumber, salesReport, qtyReport, addNumber)
 			setComment(sumFile, commentNumber, commentProspect, month+" 1st")
 			setComment(sumFile, commentNumber, commentProspect2, month+" 2nd")
 			setComment(sumFile, commentNumber, commentReport, "July Report")
 		case "September":
-			sumFile.SetCellValue("変数", "I"+calculateResult(rowNumber, 0), salesResult)
-			sumFile.SetCellValue("変数", "I"+calculateProspect(rowNumber, 9, addNumber), salesProspect)
-			sumFile.SetCellValue("変数", "I"+calculateProspect(rowNumber, 10, addNumber), qtyProspect)
-			sumFile.SetCellValue("変数", "I"+calculateResult(rowNumber, 26), salesResult2)
-			sumFile.SetCellValue("変数", "I"+calculateProspect(rowNumber, 35, addNumber), salesProspect2)
-			sumFile.SetCellValue("変数", "I"+calculateProspect(rowNumber, 36, addNumber), qtyProspect2)
-			sumFile.SetCellValue("変数", "H"+calculateProspect(rowNumber, 52, addNumber), salesReport)
-			sumFile.SetCellValue("変数", "H"+calculateProspect(rowNumber, 53, addNumber), qtyReport)
+			setProspect(sumFile, "I", rowNumber, salesResult, salesProspect, qtyProspect, addNumber)
+			setProspect2(sumFile, "I", rowNumber, salesResult2, salesProspect2, qtyProspect2, addNumber)
+			setReport(sumFile, "H", rowNumber, salesReport, qtyReport, addNumber)
 			setComment(sumFile, commentNumber, commentProspect, month+" 1st")
 			setComment(sumFile, commentNumber, commentProspect2, month+" 2nd")
 			setComment(sumFile, commentNumber, commentReport, "August Report")
 		case "October":
-			sumFile.SetCellValue("変数", "J"+calculateResult(rowNumber, 0), salesResult)
-			sumFile.SetCellValue("変数", "J"+calculateProspect(rowNumber, 9, addNumber), salesProspect)
-			sumFile.SetCellValue("変数", "J"+calculateProspect(rowNumber, 10, addNumber), qtyProspect)
-			sumFile.SetCellValue("変数", "J"+calculateResult(rowNumber, 26), salesResult2)
-			sumFile.SetCellValue("変数", "J"+calculateProspect(rowNumber, 35, addNumber), salesProspect2)
-			sumFile.SetCellValue("変数", "J"+calculateProspect(rowNumber, 36, addNumber), qtyProspect2)
-			sumFile.SetCellValue("変数", "I"+calculateProspect(rowNumber, 52, addNumber), salesReport)
-			sumFile.SetCellValue("変数", "I"+calculateProspect(rowNumber, 53, addNumber), qtyReport)
+			setProspect(sumFile, "J", rowNumber, salesResult, salesProspect, qtyProspect, addNumber)
+			setProspect2(sumFile, "J", rowNumber, salesResult2, salesProspect2, qtyProspect2, addNumber)
+			setReport(sumFile, "I", rowNumber, salesReport, qtyReport, addNumber)
 			setComment(sumFile, commentNumber, commentProspect, month+" 1st")
 			setComment(sumFile, commentNumber, commentProspect2, month+" 2nd")
 			setComment(sumFile, commentNumber, commentReport, "September Report")
 		case "November":
-			sumFile.SetCellValue("変数", "K"+calculateResult(rowNumber, 0), salesResult)
-			sumFile.SetCellValue("変数", "K"+calculateProspect(rowNumber, 9, addNumber), salesProspect)
-			sumFile.SetCellValue("変数", "K"+calculateProspect(rowNumber, 10, addNumber), qtyProspect)
-			sumFile.SetCellValue("変数", "K"+calculateResult(rowNumber, 26), salesResult2)
-			sumFile.SetCellValue("変数", "K"+calculateProspect(rowNumber, 35, addNumber), salesProspect2)
-			sumFile.SetCellValue("変数", "K"+calculateProspect(rowNumber, 36, addNumber), qtyProspect2)
-			sumFile.SetCellValue("変数", "J"+calculateProspect(rowNumber, 52, addNumber), salesReport)
-			sumFile.SetCellValue("変数", "J"+calculateProspect(rowNumber, 53, addNumber), qtyReport)
+			setProspect(sumFile, "K", rowNumber, salesResult, salesProspect, qtyProspect, addNumber)
+			setProspect2(sumFile, "K", rowNumber, salesResult2, salesProspect2, qtyProspect2, addNumber)
+			setReport(sumFile, "J", rowNumber, salesReport, qtyReport, addNumber)
 			setComment(sumFile, commentNumber, commentProspect, month+" 1st")
 			setComment(sumFile, commentNumber, commentProspect2, month+" 2nd")
 			setComment(sumFile, commentNumber, commentReport, "October Report")
 		case "December":
-			sumFile.SetCellValue("変数", "L"+calculateResult(rowNumber, 0), salesResult)
-			sumFile.SetCellValue("変数", "L"+calculateProspect(rowNumber, 9, addNumber), salesProspect)
-			sumFile.SetCellValue("変数", "L"+calculateProspect(rowNumber, 10, addNumber), qtyProspect)
-			sumFile.SetCellValue("変数", "L"+calculateResult(rowNumber, 26), salesResult2)
-			sumFile.SetCellValue("変数", "L"+calculateProspect(rowNumber, 35, addNumber), salesProspect2)
-			sumFile.SetCellValue("変数", "L"+calculateProspect(rowNumber, 36, addNumber), qtyProspect2)
-			sumFile.SetCellValue("変数", "K"+calculateProspect(rowNumber, 52, addNumber), salesReport)
-			sumFile.SetCellValue("変数", "K"+calculateProspect(rowNumber, 53, addNumber), qtyReport)
+			setProspect(sumFile, "L", rowNumber, salesResult, salesProspect, qtyProspect, addNumber)
+			setProspect2(sumFile, "L", rowNumber, salesResult2, salesProspect2, qtyProspect2, addNumber)
+			setReport(sumFile, "K", rowNumber, salesReport, qtyReport, addNumber)
 			setComment(sumFile, commentNumber, commentProspect, month+" 1st")
 			setComment(sumFile, commentNumber, commentProspect2, month+" 2nd")
 			setComment(sumFile, commentNumber, commentReport, "November Report")
 		case "January":
-			sumFile.SetCellValue("変数", "M"+calculateResult(rowNumber, 0), salesResult)
-			sumFile.SetCellValue("変数", "M"+calculateProspect(rowNumber, 9, addNumber), salesProspect)
-			sumFile.SetCellValue("変数", "M"+calculateProspect(rowNumber, 10, addNumber), qtyProspect)
-			sumFile.SetCellValue("変数", "M"+calculateResult(rowNumber, 26), salesResult2)
-			sumFile.SetCellValue("変数", "M"+calculateProspect(rowNumber, 35, addNumber), salesProspect2)
-			sumFile.SetCellValue("変数", "M"+calculateProspect(rowNumber, 36, addNumber), qtyProspect2)
-			sumFile.SetCellValue("変数", "L"+calculateProspect(rowNumber, 52, addNumber), salesReport)
-			sumFile.SetCellValue("変数", "L"+calculateProspect(rowNumber, 53, addNumber), qtyReport)
+			setProspect(sumFile, "M", rowNumber, salesResult, salesProspect, qtyProspect, addNumber)
+			setProspect2(sumFile, "M", rowNumber, salesResult2, salesProspect2, qtyProspect2, addNumber)
+			setReport(sumFile, "L", rowNumber, salesReport, qtyReport, addNumber)
 			setComment(sumFile, commentNumber, commentProspect, month+" 1st")
 			setComment(sumFile, commentNumber, commentProspect2, month+" 2nd")
 			setComment(sumFile, commentNumber, commentReport, "December Report")
 		case "February":
-			sumFile.SetCellValue("変数", "N"+calculateResult(rowNumber, 0), salesResult)
-			sumFile.SetCellValue("変数", "N"+calculateProspect(rowNumber, 9, addNumber), salesProspect)
-			sumFile.SetCellValue("変数", "N"+calculateProspect(rowNumber, 10, addNumber), qtyProspect)
-			sumFile.SetCellValue("変数", "N"+calculateResult(rowNumber, 26), salesResult2)
-			sumFile.SetCellValue("変数", "N"+calculateProspect(rowNumber, 35, addNumber), salesProspect2)
-			sumFile.SetCellValue("変数", "N"+calculateProspect(rowNumber, 36, addNumber), qtyProspect2)
-			sumFile.SetCellValue("変数", "M"+calculateProspect(rowNumber, 52, addNumber), salesReport)
-			sumFile.SetCellValue("変数", "M"+calculateProspect(rowNumber, 53, addNumber), qtyReport)
+			setProspect(sumFile, "N", rowNumber, salesResult, salesProspect, qtyProspect, addNumber)
+			setProspect2(sumFile, "N", rowNumber, salesResult2, salesProspect2, qtyProspect2, addNumber)
+			setReport(sumFile, "M", rowNumber, salesReport, qtyReport, addNumber)
 			setComment(sumFile, commentNumber, commentProspect, month+" 1st")
 			setComment(sumFile, commentNumber, commentProspect2, month+" 2nd")
 			setComment(sumFile, commentNumber, commentReport, "January Report")
 		case "March":
-			sumFile.SetCellValue("変数", "O"+calculateResult(rowNumber, 0), salesResult)
-			sumFile.SetCellValue("変数", "O"+calculateProspect(rowNumber, 9, addNumber), salesProspect)
-			sumFile.SetCellValue("変数", "O"+calculateProspect(rowNumber, 10, addNumber), qtyProspect)
-			sumFile.SetCellValue("変数", "O"+calculateResult(rowNumber, 26), salesResult2)
-			sumFile.SetCellValue("変数", "O"+calculateProspect(rowNumber, 35, addNumber), salesProspect2)
-			sumFile.SetCellValue("変数", "O"+calculateProspect(rowNumber, 36, addNumber), qtyProspect2)
-			sumFile.SetCellValue("変数", "N"+calculateProspect(rowNumber, 52, addNumber), salesReport)
-			sumFile.SetCellValue("変数", "N"+calculateProspect(rowNumber, 53, addNumber), qtyReport)
+			setProspect(sumFile, "O", rowNumber, salesResult, salesProspect, qtyProspect, addNumber)
+			setProspect2(sumFile, "O", rowNumber, salesResult2, salesProspect2, qtyProspect2, addNumber)
+			setReport(sumFile, "N", rowNumber, salesReport, qtyReport, addNumber)
 			setComment(sumFile, commentNumber, commentProspect, month+" 1st")
 			setComment(sumFile, commentNumber, commentProspect2, month+" 2nd")
 			setComment(sumFile, commentNumber, commentReport, "February Report")
